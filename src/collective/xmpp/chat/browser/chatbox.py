7dbcb93a4ded2ed5c4c942287c8534f7e30a3909
JC Brand <jc@opkode.com>
2012-06-19 21:32:50 +0200

    Messages will be fetched via javascript.

--- src/collective/xmpp/chat/browser/chatbox.py
+++ src/collective/xmpp/chat/browser/chatbox.py
@@ -6,7 +6,6 @@ from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 
 from collective.xmpp.chat.browser.interfaces import IChatBox
-from collective.xmpp.chat import iso8601
 
 
 class ChatBox(BrowserView):
@@ -39,26 +38,13 @@ class ChatBox(BrowserView):
             return getSite().unrestrictedTraverse(contact).Title()
         return contact
 
-    def _to_local_timezone(self, messages, tzoffset):
-        tzoffset = iso8601.FixedOffset(0, int(tzoffset), '')
-        for key in messages.keys():
-            for mtuple in messages[key]:
-                # Change message times to local timezone
-                olddate = iso8601.parse_date(mtuple[2])
-                date = (olddate + tzoffset.offset).replace(tzinfo=tzoffset)
-                mtuple[2] = date.isoformat()
-        return messages
-
     def render_chat_box(self, chat_id, box_id, tzoffset, jid):
         """ """
         chat_type, audience = chat_id.split('_', 1)
-        # TODO: Get message history
-        messages = {}
-        messages = self._to_local_timezone(messages, tzoffset)
         return self.template(
-                        messages=messages, 
                         audience=audience,
                         jid=jid,
                         box_id=box_id, 
                         chat_type=chat_type,
                         chat_id=chat_id ,)
+
