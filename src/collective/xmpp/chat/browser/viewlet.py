2597983f5194b9e7460b6b5635235dbe2d411e88
JC Brand <jc@opkode.com>
2012-10-16 23:09:19 +0200

    Also include auto_subscribe in the chat data viewlet.

---
+++ src/collective/xmpp/chat/browser/viewlet.py
@@ -0,0 +1,18 @@
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from plone.app.layout.viewlets.common import ViewletBase
+from Products.CMFCore.utils import getToolByName
+
+
+class ChatData(ViewletBase):
+    """ """
+
+    def __init__(self, context, request, view, manager):
+        super(ChatData, self).__init__(context, request, view, manager)
+        pm = getToolByName(context, 'portal_membership')
+        member = pm.getAuthenticatedMember()
+        self.username = member.getId()
+        info = pm.getMemberInfo(self.username)
+        self.fullname = info.get('fullname', self.username)
+        registry = getUtility(IRegistry)
+        self.auto_subscribe = registry['collective.xmpp.autoSubscribe']
