afe422191f3321395581dc4c4befee6bdce1d338
JC Brand <jc@opkode.com>
2013-02-02 13:46:18 +0200

    Fix version num

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import os
 
 use_setuptools()
-version = '0.2b3opkode1'
+version = '0.2b4dev0'
 
 setup(name='collective.xmpp.chat',
       version=version,
