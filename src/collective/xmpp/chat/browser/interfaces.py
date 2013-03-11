8b3b7ceca6f981872542e42180d3e2dcb4cada85
JC Brand <jc@opkode.com>
2013-03-11 20:05:24 +0200

    Remove unused interface IChatBox

--- src/collective/xmpp/chat/browser/interfaces.py
+++ src/collective/xmpp/chat/browser/interfaces.py
@@ -1,10 +1,6 @@
 from zope.interface import Interface
 
-class IXMPPChatLayer(Interface):
-    """Marker Interface for a custom BrowserLayer
-    """
 
-class IChatBox(Interface):
-    """ """
-    def render_chat_box(self, box_id, user, contact):
-        """ """
+class IXMPPChatLayer(Interface):
+    """ Marker Interface for a custom BrowserLayer
+    """
