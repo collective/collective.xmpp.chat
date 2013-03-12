b731751778da346c300e5094d7c9c4c7162c0cdf
JC Brand <jc@opkode.com>
2013-03-12 12:20:59 +0200

    Flake*

--- setup.py
+++ setup.py
@@ -5,41 +5,43 @@ import os
 use_setuptools()
 version = '0.2dev0'
 
-setup(name='collective.xmpp.chat',
-      version=version,
-      description="XMPP-based instant Messaging for Plone",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
+setup(
+    name='collective.xmpp.chat',
+    version=version,
+    description="XMPP-based instant Messaging for Plone",
+    long_description=open("README.rst").read() + "\n" +
+                open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Programming Language :: Python",
+    ],
+    keywords='xmpp chat messaging',
+    author='JC Brand',
+    author_email='jc@opkode.com',
+    url='http://svn.plone.org/svn/collective/',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['collective', 'collective.xmpp'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'collective.xmpp.core',
+    ],
+    extras_require={
+        "sphinx": [
+            "Sphinx>=1.0",
+            "repoze.sphinx.autointerface",
         ],
-      keywords='xmpp chat messaging',
-      author='JC Brand',
-      author_email='jc@opkode.com',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages('src', exclude=['ez_setup']),
-      package_dir={'': 'src'},
-      namespace_packages=['collective', 'collective.xmpp'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-            'setuptools',
-            'collective.xmpp.core',
-      ],
-      extras_require = {
-        "sphinx": [ "Sphinx >=1.0",
-                    "repoze.sphinx.autointerface",
-                  ],
         "test": ['plone.app.testing[robot]'],
-      },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript", "setuptools_git >= 0.3"],
-      paster_plugins=["ZopeSkel"],
-      )
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=["PasteScript", "setuptools_git >= 0.3"],
+    paster_plugins=["ZopeSkel"],
+)
