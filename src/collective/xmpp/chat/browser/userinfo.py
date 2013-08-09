f74bd1c25ae7c2fd3d916d497f6bd61bb8ab57ff
ichim-david <ichim.david@gmail.com>
2013-08-09 09:32:01 +0300

    Added fixes regarding user search when using plone.pas from the 0.2.x branch

--- src/collective/xmpp/chat/browser/userinfo.py
+++ src/collective/xmpp/chat/browser/userinfo.py
@@ -2,6 +2,8 @@ from Products.Five import BrowserView
 from collective.xmpp.core.browser import userinfo
 from plone.app.controlpanel.usergroups import UsersOverviewControlPanel
 import json
+import logging
+log = logging.getLogger(__name__)
 
 
 class XMPPUserDetails(userinfo.XMPPUserDetails):
@@ -18,4 +20,19 @@ class SearchUsers(BrowserView):
         if len(searchtext) < 2:
             return []
         panel = UsersOverviewControlPanel(self.context, self.request)
-        return json.dumps(panel.doSearch(searchtext))
+        users = panel.doSearch(searchtext)
+        # complete name and surname can contain badly encoded values
+        # when returned from plone.pas search therefore we try to convert
+        # them to unicode with the latin characters encoding
+        fields = ['cn', 'sn']
+        for user_dict in users:
+            for field in fields:
+                try:
+                    field_value = user_dict.get(field)
+                    if field_value:
+                        user_dict[field] = unicode(field_value, "ISO-8859-1")
+                except UnicodeDecodeError:
+                    users.remove(user_dict)
+                    log.warn("Can't decode %s", user_dict[field])
+
+        return json.dumps(users)
