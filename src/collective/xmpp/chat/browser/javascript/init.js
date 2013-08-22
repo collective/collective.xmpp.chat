64941ea19195108dda4fc632520f42cfb3f2b9bc
JC Brand <jc@opkode.com>
2013-08-23 00:59:43 +0200

    Newest converse.js requires the connection obj be passed in.

--- src/collective/xmpp/chat/browser/javascript/init.js
+++ src/collective/xmpp/chat/browser/javascript/init.js
@@ -1,16 +1,17 @@
 $('#chatpanel').ready(function () {
-    var $chatdata = $('#collective-xmpp-chat-data');
-    converse.initialize({
-        animate: true,
-        prebind: true,
-        xhr_user_search: true,
-        auto_subscribe: $chatdata.attr('auto_subscribe'),
-        auto_list_rooms: true,
-        hide_muc_server: true,
-        i18n: window.locales[$chatdata.attr('lang')||'en']
-    });
     $(document).unbind('jarnxmpp.connected');
     $(document).bind('jarnxmpp.connected', function (ev, connection) {
-        converse.onConnected(connection);
+        var $chatdata = $('#collective-xmpp-chat-data');
+        converse.initialize({
+            animate: true,
+            prebind: true,
+            connection: connection,
+            xhr_user_search: true,
+            auto_subscribe: $chatdata.attr('auto_subscribe'),
+            auto_list_rooms: true,
+            hide_muc_server: true,
+            i18n: window.locales[$chatdata.attr('lang')||'en'],
+            debug: true
+        });
     });
 });
