be400810cf7e47168cee407b09173f6ccc21152c
JC Brand <jc@opkode.com>
2014-01-30 11:28:34 +0200

    Compat fix. The default markup required by converse.js has changed.

--- src/collective/xmpp/chat/browser/javascript/init.js
+++ src/collective/xmpp/chat/browser/javascript/init.js
@@ -1,16 +1,15 @@
-$('#chatpanel').ready(function () {
+$('#conversejs').ready(function () {
     $(document).unbind('jarnxmpp.connected');
     $(document).bind('jarnxmpp.connected', function (ev, connection) {
-        var $chatdata = $('#collective-xmpp-chat-data');
         converse.initialize({
             animate: true,
             prebind: true,
             connection: connection,
             xhr_user_search: true,
-            auto_subscribe: $chatdata.attr('auto_subscribe'),
+            auto_subscribe: $(this).data('autosubscribe'),
             auto_list_rooms: true,
             hide_muc_server: true,
-            i18n: window.locales[$chatdata.attr('lang')||'en'],
+            i18n: window.locales[$(this).data('lang')||'en'],
             debug: true
         });
     });
